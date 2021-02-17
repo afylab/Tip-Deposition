54234dd311a65194d2278505c06646c9418136fd
Trevor Arp <trevor.arp@email.ucr.edu>
2021-02-17 10:31:18 -0800

    New recipes structure

--- Recipes/recipe.py
+++ Recipes/recipe.py
@@ -1,3 +1,29 @@
 '''
 A module for defining tip deposition recipes generically
 '''
+
+class Recipe():
+    '''
+    Generic class for a specific recipe for tip deposition, defines a particular ordering
+    of steps as well as any special behavior that is needed to carry out the recipe.
+
+    Main function to inherit and override is proceed() which is a generator that contains
+    each step before a yield keyword.
+    '''
+    def __init__(self):
+        '''
+        '''
+        pass
+    #
+
+    '''
+    Checks that all the equipment needed to carry out the recipie
+    '''
+    def equipment_check(self, servers):
+        pass
+        # Identify all the keys in the instruction set and check that they are in servers
+    #
+#
+
+if __name__ == '__main__':
+    pass
--- TipDeposition.py
+++ TipDeposition.py
@@ -9,10 +9,31 @@ class Sequencer():
     Backend of the tip deposition that translates a tip deposition recipe into a sequence
     of hardware commands, checking user input
     '''
-    def __init__(self, recipe, servers, gui):
+    def __init__(self, recipe, servers):
         '''
+        Setup the recipe.
+
+        Args:
+            recipe : The recipe object to load
+            servers : A dictionary ordered with 'equipment-name':labRAD-Server-Reference. The
+                key will be used as a generic key to lookup the hardware from Sequencer.servers[key]
         '''
-        pass
+        self.active = False # Parameter is active when a tip is being deposited
+
+        self.recipe = recipe
+        self.servers = servers
+
+        # Check that the recipe has all the equipment it needs
+        if not self.recipe.equipment_check(self.servers):
+            # Manage equipment
+            pass
+
+        # Initlize the process window
+        # self.gui = ProcessWindow
+
+        # Give the
+
+        # Setup the equipment
     #
 #
 
