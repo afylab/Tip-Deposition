1ac0ff4388aed4b288f82676ffa63faf4e63c0f1
Trevor Arp <trevorarp@ucsb.edu>
2021-04-19 18:17:30 -0700

    Some basic functions added to Cryo_Thermal_Evaportation

--- Recipes/Cryo_Thermal_Evaporation.py
+++ Recipes/Cryo_Thermal_Evaporation.py
@@ -6,7 +6,51 @@ A simple thermal evaporation using only the thermal evaporator and the cryogenic
 from recipe import Recipe, Step
 
 class Cryo_Thermal_Evaporation(Recipe):
+    def __init__(self, equip):
+        # Add all the hardwave servers needed for evaporation, including 'data_vault'
+        # Starting iwth the servers you always need, including Data Vault
+        servers = ['data_vault', 'stepper_server', 'rvc_server', 'valve_relay_server', 'ftm_server', 'turbo450']
+        # Then evaporation specific servers
+        servers.append(['power_supply_server'])
+        #servers = ['data_vault'] # FOR DEBUGGING
+        super().__init__(equip, required_servers=servers, version="1.0.0")
+    #
+
     def proceed(self):
+        '''
+        Initilize Servers with any setup needed.
+
+        Following the initilization from the old Evaporator software, with any comments
+        '''
+        self.command('rvc_server', 'select_device')
+        self.command('stepper_server', 'select_device')
+        self.command('ftm_server', 'select_device')
+        self.command('turbo450', 'select_device')
+
+        # Setup the power supply server
+        self.command('power_supply_server', 'select_device')
+        self.command('power_supply_server', 'adr', '6')
+        self.command('power_supply_server', 'rmt_set', 'REM')
+
+        self.command('valve_relay_server', 'select_device')
+        #Iden command added so that arduino will respond to first command given from GUI.
+        #Lack of response is somehow connected to dsrdtr port connection, but not yet sure how...
+        self.command('valve_relay_server', 'iden')
+
+        '''
+        Track the variables
+        '''
+        self.trackVariable('Pressure', 'rvc_server', 'get_nom_prs')
+        self.trackVariable('Deposition Rate', 'ftm_server', 'get_sensor_rate')
+        self.trackVariable('Thickness', 'ftm_server', 'get_sensor_thickness')
+        self.trackVariable('Voltage (V)', 'power_supply_server', 'volt_read')
+        self.trackVariable('Temperature (C)', 'turbo450', 'temperature')
+        self.trackVariable('Frequency (Hz)', 'turbo450', 'csfrequency')
+        self.trackVariable('Current (0.1 A)', 'turbo450', 'mcurrent')
+
+        '''
+        Get parameters from the user
+        '''
         instruct = "Follow instructions for tip loading, confirm that:"
         instruct += "\n 1. The tip is loaded"
         instruct += "\n 2. The evaporation boat has been loaded with 6-7 pellets of superconductor."
@@ -28,24 +72,44 @@ class Cryo_Thermal_Evaporation(Recipe):
         step1.add_input_param("Voffset", default=self.default("Voffset"), limits=(0,10))
         yield step1
 
-        #params = step1.get_all_params()
+        params = step1.get_all_params()
         #print(params['Deposition Rate (A/s)'])
 
         step2 = Step(True, "Record the Crystal Life")
         step2.add_input_param("Crystal Life", limits=(0,100))
         yield step2
 
-        # Acess data from step1 and prepare
+        # Access data from step1 and initilize servers with information
 
         yield Step(False, "Beginning pump out sequence, waiting until pressure falls below 1E-6 mbar.")
 
-        # Perform pump down sequence
+        '''
+        DEV NOTE: Check the valve server, it has return values that will need to be adjusted
+        to our scheme for monitoring valve status.
+        '''
 
-        yield Step(True, "Ready for cooldown, follow cooldown instructions the press proceed.")
+        # Open all the valves
+        self.command('valve_relay_server', 'gate_open')
+        self.command('valve_relay_server', 'chamber_valve_open')
+        self.command('valve_relay_server', 'turbo_valve_open')
+
+        # First rough out the chamber with the scroll pump
+        self.command('valve_relay_server', 'scroll_on')
+        self.wait_until('Pressure', 1e-1, "less than")
+
+        # Close the Chamber valve
+        self.command('valve_relay_server', 'chamber_valve_close')
+        self.command('valve_relay_server', 'turbo_on')
+
+
+        yield Step(True, "Ready for cooldown, follow cooldown instructions then press proceed.")
 
         yield Step(False, "Waiting until stable temperature is reached.")
 
         # Wait until base temperature is reached.
+        self.wait_until('Temperature (C)', 7, "less than")
+
+        yield Step(True, "Adjust the helium flow to acheive desired stability.")
 
         # Calibrate the voltage needed to reach set deposition rate
         yield Step(True, "Rotate Tip to 165&deg;.")
--- Servers/FTM_2400.py
+++ Servers/FTM_2400.py
@@ -42,8 +42,8 @@ import labrad.units as units
 from labrad.types import Value
 import time
 
-TIMEOUT = Value(5,'s') 
-# This server does not use timeout because the device doesn't implement standard message termination. 
+TIMEOUT = Value(5,'s')
+# This server does not use timeout because the device doesn't implement standard message termination.
 BAUD    = 19200
 BYTESIZE = 8
 STOPBITS = 1
@@ -67,11 +67,11 @@ class FTMWrapper(DeviceWrapper):
         p.timeout(TIMEOUT)
         p.read()  # clear out the read buffer
 
-        #No timeout means checks for stored characters then immediately stops waiting. 
+        #No timeout means checks for stored characters then immediately stops waiting.
         p.timeout(None)
         print(" CONNECTED ")
         yield p.send()
-        
+
     def packet(self):
         """Create a packet in our private context"""
         return self.server.packet(context=self.ctx)
@@ -128,11 +128,11 @@ class FTMServer(DeviceServer):
         print("printing all the keys",keys)
         for k in keys:
             print("k=",k)
-            p.get(k, key=k)            
+            p.get(k, key=k)
         ans = yield p.send()
         print("ans=",ans)
         self.serialLinks = dict((k, ans[k]) for k in keys)
-    
+
     @inlineCallbacks
     def findDevices(self):
         devs = []
@@ -148,51 +148,51 @@ class FTMServer(DeviceServer):
             devName = '%s - %s' % (serServer, port)
             devs += [(devName, (server, port))]
         returnValue(devs)
-        
-    @setting(100,command='s',returns='s')    
+
+    @setting(100,command='s',returns='s')
     def format_command(self,c,command):
         """For given input, formats the command as expected by the FTM-2400"""
         command = '!' + chr(len(command)+34) + command
         crc_val = self.calcCRC(c,command)
         command = command + chr(self.crc1(c,crc_val)) + chr(self.crc2(c,crc_val))
         return command
-    
-    @setting(101,str='s',returns='i')    
+
+    @setting(101,str='s',returns='i')
     def calcCRC(self,c,str):
         """For given outgoing string, calculates the appropriate crc value ignoring the first character,
         which should simply be the sync character (in this case '!') """
-        crc = 0    
-        length = 1 + ord(str[1]) - 34    
+        crc = 0
+        length = 1 + ord(str[1]) - 34
         if length > 0:
-            crc = 0x3fff        
+            crc = 0x3fff
             for jx in range(1,length+1):
-                crc = crc ^ ord(str[jx])            
+                crc = crc ^ ord(str[jx])
                 for ix in range(0,8):
                     tmpCRC = crc
                     crc = crc>>1
                     if tmpCRC & 0x1 == 1:
-                        crc = crc ^ 0x2001                
-                crc = crc & 0x3fff    
+                        crc = crc ^ 0x2001
+                crc = crc & 0x3fff
         return crc
-    
-    @setting(102,crc='i',returns='i') 
+
+    @setting(102,crc='i',returns='i')
     def crc2(self,c,crc):
         """From the CRC value, compute the number associated with the appropriate ASCII character
         that goes second."""
         val = (((crc >> 7)& 0x7f) + 34)
         return val
-    
-    @setting(103,crc='i',returns='i') 
+
+    @setting(103,crc='i',returns='i')
     def crc1(self,c,crc):
         """From the CRC value, compute the number associated with the appropriate ASCII character
         that goes first."""
         val = ((crc & 0x7f)+34)
         return val
-        
+
     @setting(104,ans = 's',returns = 'b')
     def check_ans(self,c,ans):
         """Checks that the returned answer indicates a successful communication and is complete"""
-        try: 
+        try:
             if ans[2] != 'A':
                 if ans[2] == 'C':
                     print('Invalid Command')
@@ -202,38 +202,38 @@ class FTMServer(DeviceServer):
         except IndexError:
             #print 'Message status character not yet arrived'
             return False
-            
+
         try:
             crc_val = self.calcCRC_in(c,ans[:-2])
             if chr(self.crc1(c,crc_val)) + chr(self.crc2(c,crc_val))== ans[-2:]:
                 return True
-            else:    
+            else:
                 print('CRC did not match expected form. Error in data.')
                 return False
         except IndexError:
             #print 'Entire string not yet arrived'
             return False
-        
 
-            
-    @setting(105,str='s',returns='i')    
+
+
+    @setting(105,str='s',returns='i')
     def calcCRC_in(self,c,str):
         """For given incoming string, calculates the appropriate crc value ignoring the first character,
         which should simply be the sync character (in this case '!') """
-        crc = 0    
-        length = 1 + ord(str[1]) - 35    
+        crc = 0
+        length = 1 + ord(str[1]) - 35
         if length > 0:
-            crc = 0x3fff        
+            crc = 0x3fff
             for jx in range(1,length+1):
-                crc = crc ^ ord(str[jx])            
+                crc = crc ^ ord(str[jx])
                 for ix in range(0,8):
                     tmpCRC = crc
                     crc = crc>>1
                     if tmpCRC & 0x1 == 1:
-                        crc = crc ^ 0x2001                
-                crc = crc & 0x3fff    
+                        crc = crc ^ 0x2001
+                crc = crc & 0x3fff
         return crc
-        
+
     @setting(106,input = 's', returns = 's')
     def read(self,c, input):
         """This piece of equipment doesn't use carriage returns, so the serial port cannot recognize
@@ -269,44 +269,44 @@ class FTMServer(DeviceServer):
                 returnValue("Timeout")
             yield self.sleep(0.1)
             #print "Sleeping"
-                     
+
     def sleep(self,secs):
         """Asynchronous compatible sleep command. Sleeps for given time in seconds, but allows
         other operations to be done elsewhere while paused."""
         d = defer.Deferred()
         reactor.callLater(secs,d.callback,'Sleeping')
         return d
-                
+
     @setting(200,returns='s')
     def get_ver(self,c):
         """Queries the @ command and returns the response. Usage is get_ver()"""
         ans = yield self.read(c,'@')
         returnValue(ans)
-            
+
     @setting(201,film = 'i', returns='s')
     def get_film_parameters(self,c, film):
         """Queries the L command and returns the response. Usage is get_rate()"""
         ans = yield self.read(c,'A'+str(film)+'?')
         returnValue(ans)
-            
+
     @setting(202,film = 'i',film_name = 's',density = 'v[]',tool = 'i',
                     zfactor = 'v[]', thickness = 'v[]', thickness_setpoint = 'v[]',
                     time_setpoint = 'i', sensor = 'i',returns='s')
-    def set_film_parameters(self,c, film, film_name, density, tool, 
+    def set_film_parameters(self,c, film, film_name, density, tool,
                 zfactor, thickness, thickness_setpoint, time_setpoint, sensor):
         """Queries the L command and returns the response. Usage is get_rate()"""
-        ans = yield self.read(c,'A'+str(film)+film_name + ' ' + str(density) 
+        ans = yield self.read(c,'A'+str(film)+film_name + ' ' + str(density)
                 + ' ' + str(tool) + ' ' + str(zfactor) + ' ' + str(thickness) + ' ' +
                 str(thickness_setpoint) + ' '+ str(time_setpoint) + ' ' + str(sensor))
         returnValue(ans)
-            
+
     @setting(203,returns='s')
     def get_sys1_parameters(self,c, film):
         """Queries the L command and returns the response. Usage is get_rate()"""
         ans = yield self.read(c,'B?')
         returnValue(ans)
-            
-    # Add set sys1 parameters if necessary eventually. Didn't seem necessary.         
+
+    # Add set sys1 parameters if necessary eventually. Didn't seem necessary.
     # @setting(204,returns='s')
     # def set_sys1_parameters(self,c, film):
         # """Queries the L command and returns the response. Usage is get_rate()"""
@@ -314,122 +314,124 @@ class FTMServer(DeviceServer):
         # command = self.format_command(c,'B')
         # yield dev.write(command)
         # ans = yield dev.read()
-        
+
         # if self.check_ans(c,ans):
             # returnValue(ans[3:-2])
-            
+
     @setting(205,returns='s')
     def get_sys2_parameters(self,c):
         """Queries the L command and returns the response. Usage is get_rate()"""
         ans = yield self.read(c,'C?')
         returnValue(ans)
-            
+
     @setting(206,min_freq = 'v[]', max_freq = 'v[]', min_rate = 'v[]', max_rate = 'v[]',
             min_thick = 'v[]', max_thick = 'v[]', etch_mode = 'i', returns='s')
     def set_sys2_parameters(self,c,min_freq,max_freq,min_rate,max_rate,min_thick,
                 max_thick,etch_mode):
         """Queries the L command and returns the response. Usage is get_rate()"""
-        ans = yield self.read(c,'C' + ' ' + str(min_freq) + ' ' + str(max_freq) + ' ' + 
-            str(min_rate) + ' ' + str(max_rate) + ' ' + str(min_thick) + ' ' + str(max_thick) + 
+        ans = yield self.read(c,'C' + ' ' + str(min_freq) + ' ' + str(max_freq) + ' ' +
+            str(min_rate) + ' ' + str(max_rate) + ' ' + str(min_thick) + ' ' + str(max_thick) +
             ' ' + str(etch_mode))
         returnValue(ans)
-            
+
     @setting(207,film = 'i',returns='s')
     def set_film(self,c, film):
         """Queries the L command and returns the response. Usage is get_rate()"""
         ans = yield self.read(c,'D'+str(film))
         returnValue(ans)
-            
+
     @setting(208,returns='s')
     def get_num_channels(self,c):
         """Queries the L command and returns the response. Usage is get_rate()"""
         ans = yield self.read(c,'J')
         returnValue(ans)
-            
-    @setting(209,sensor = 'i',returns='s')
-    def get_sensor_rate(self,c,sensor):
+
+    #@setting(209,sensor = 'i',returns='s')
+    @setting(209,returns='s')
+    def get_sensor_rate(self,c,sensor=1):
         """Queries the L command and returns the response. Usage is get_rate()"""
         ans = yield self.read(c,'L'+str(sensor)+'?')
         returnValue(ans)
-            
+
     @setting(210,returns='s')
     def get_avg_rate(self,c,sensor):
         """Queries the L command and returns the response. Usage is get_rate()"""
         ans = yield self.read(c,'M')
         returnValue(ans)
 
-    @setting(211,sensor = 'i',returns='s')
-    def get_sensor_thickness(self,c,sensor):
+    #@setting(211,sensor = 'i',returns='s')
+    @setting(211,returns='s')
+    def get_sensor_thickness(self,c,sensor=1):
         """Queries the L command and returns the response. Usage is get_rate()"""
         ans = yield self.read(c,'N'+str(sensor)+'?')
-        returnValue(ans)      
+        returnValue(ans)
 
     @setting(212,returns='s')
     def get_avg_thickness(self,c):
         """Queries the L command and returns the response. Usage is get_rate()"""
         ans = yield self.read(c,'O')
-        returnValue(ans)  
+        returnValue(ans)
 
     @setting(213,sensor = 'i',returns='s')
     def get_sensor_freq(self,c,sensor):
         """Queries the L command and returns the response. Usage is get_rate()"""
         ans = yield self.read(c,'P'+str(sensor))
-        returnValue(ans)    
+        returnValue(ans)
 
     @setting(214,sensor = 'i', returns='s')
     def get_sensor_life(self,c,sensor):
         """Queries the L command and returns the response. Usage is get_rate()"""
         ans = yield self.read(c,'R'+str(sensor))
-        returnValue(ans)  
+        returnValue(ans)
 
     @setting(215,returns='s')
     def zero_rates_thickness(self,c):
         """Queries the S command and returns the response. Usage is zero_rates_thickness()"""
         ans = yield self.read(c,'S')
         returnValue(ans)
-            
+
     @setting(216,returns='s')
     def zero_time(self,c):
         """Queries the L command and returns the response. Usage is get_rate()"""
         ans = yield self.read(c,'T')
         returnValue(ans)
-            
+
     @setting(217,returns='s')
     def get_shutter_status(self,c):
         """Queries the L command and returns the response. Usage is get_rate()"""
         ans = yield self.read(c,'U?')
         returnValue(ans)
-    
+
     @setting(218,returns='s')
     def set_shutter_open(self,c):
         """Queries the L command and returns the response. Usage is get_rate()"""
         ans = yield self.read(c,'U1')
         returnValue(ans)
-    
+
     @setting(219,returns='s')
     def set_shutter_close(self,c):
         """Queries the L command and returns the response. Usage is get_rate()"""
         ans = yield self.read(c,'U0')
         returnValue(ans)
-            
+
     @setting(220,returns='s')
     def get_all_sensor_data(self,c):
         """Queries the L command and returns the response. Usage is get_rate()"""
         ans = yield self.read(c,'W')
-        returnValue(ans) 
-            
+        returnValue(ans)
+
     @setting(221,returns='s')
     def power_up_status(self,c):
         """Queries the L command and returns the response. Usage is get_rate()"""
         ans = yield self.read(c,'Y')
         returnValue(ans)
-            
+
     @setting(222,returns='s')
     def set_default_parameters(self,c):
         """Queries the L command and returns the response. Usage is get_rate()"""
         ans = yield self.read(c,'Z')
         returnValue(ans)
-            
+
 __server__ = FTMServer()
 if __name__ == '__main__':
     from labrad import util
